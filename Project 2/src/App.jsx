ece7d114f6178df3e17cb46986bd545c34b96329
Pravin <avacharpravin123@gmail.com>
2025-12-05 18:19:58 +0530

    2nd project started

---
+++ Project 2/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navigation from './components/Navigation/Navigation'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navigation/>
+    </>
+  )
+}
+
+export default App
